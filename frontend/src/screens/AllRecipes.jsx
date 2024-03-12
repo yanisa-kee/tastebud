905ddaee369d1aaf0c45b6e154015e8b08a1aee2
Anthoney Xie <anthoneyx@g.ucla.edu>
2024-03-11 20:14:36 -0700

    called recipegrid in screen

--- frontend/src/screens/AllRecipes.jsx
+++ frontend/src/screens/AllRecipes.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import Recipegrid from "../components/Recipegrid.jsx";
 export default function AllRecipes() {
     return (
         <div>
             <h1>this is our all recipes</h1>
+            <Recipegrid/>
         </div>
     ) 
 }
